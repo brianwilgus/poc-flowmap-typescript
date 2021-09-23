58d4b492682c1f06c8008134bd77b7843f80cb4a
Brian Wilgus <brianawilgus@gmail.com>
2021-09-23 09:53:25 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { StaticMap } from 'react-map-gl'
+import FlowMapLayer from '@flowmap.gl/core'
+import { DeckGL } from '@deck.gl/react';
+import geoViewport from '@mapbox/geo-viewport'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+const MAPBOX_TOKEN = process.env.REACT_APP_MapboxAccessToken
+
+const getInitialViewState = () => {
+  const bbox = [5.956453645364537, 45.818, 10.492, 47.808]
+  const { center: [longitude, latitude], zoom } =
+    geoViewport.viewport(
+      bbox,
+      [window.innerWidth, window.innerHeight],
+      undefined, undefined, 512
+    )
+  return {
+    longitude,
+    latitude,
+    zoom,
+    bearing: 0,
+    pitch: 0,
+  }
 }
 
-export default App;
+const colors = {
+  flows: {
+    scheme: [
+      // Teal scheme from https://carto.com/carto-colors/
+      '#d1eeea','#a8dbd9','#85c4c9','#68abb8','#4f90a6','#3b738f','#2a5674'
+    ],
+  },
+  locationAreas: {
+    outline: 'rgba(92,112,128,0.5)',
+    normal: 'rgba(187,187,187,0.5)',
+    selected: 'rgba(217,130,43,0.5)',
+  },
+}
+
+
+
+export default class App extends Component {
+  state = {
+    locations: null,
+    flows: null,
+  }
+
+  componentDidMount() {
+    fetch(`${process.env.PUBLIC_URL}/data/locations.json`)
+      .then(response => response.json())
+      .then(json => this.setState({ locations: json }))
+
+    fetch(`${process.env.PUBLIC_URL}/data/flows.json`)
+      .then(response => response.json())
+      .then(json => this.setState({ flows: json }))
+  }
+
+  render() {
+    const { locations, flows } = this.state
+    const layers = []
+    if (locations && flows) {
+      layers.push(
+        new FlowMapLayer({
+          id: 'flowMapLayer',
+          colors,
+          locations,
+          flows,
+          getLocationId: l => l.properties.abbr,
+          getLocationCentroid: l => l.properties.centroid,
+          getFlowOriginId: f => f.origin,
+          getFlowDestId: f => f.dest,
+          getFlowMagnitude: f => f.count,
+        })
+      )
+    }
+
+    return (
+      <DeckGL
+        initialViewState={getInitialViewState()}
+        controller={true}
+        layers={layers}
+      >
+        <StaticMap mapboxApiAccessToken={MAPBOX_TOKEN} />
+      </DeckGL>
+    )
+  }
+}
